8242d9e86f629eb4b85e383c1790bd237859f49c
DenisKaminsky <deniskaminsky123@mail.ru>
2018-12-13 21:36:08 +0300

    add ball physics

--- Pong Game/Gameplay.cpp
+++ Pong Game/Gameplay.cpp
@@ -6,23 +6,60 @@
 Graphics* graphics;
 HWND hWnd;
 
-int posX = 0;
-int posY = 0;
+//ball
+POINT ballPosition;
+POINT ballSpeed;
+int ballRadius = 50;
+
+int windowHeight = 0;
+int windowWidth = 0;
+
+void BallBounceFromBoundary()
+{
+	if (ballPosition.y + ballRadius >= windowHeight)
+		ballSpeed.y = -ballSpeed.y;
+	if (ballPosition.y - ballRadius <= 0)
+		ballSpeed.y = -ballSpeed.y;
+}
+
+void BallOutOfField()
+{
+	if ((ballPosition.x + ballRadius >= windowWidth) || (ballPosition.x - ballRadius <= 0))
+	{		
+		ballPosition.x = windowWidth / 2;
+		ballPosition.y = windowHeight / 2;
+		Sleep(1000);
+	}
+}
 
 VOID CALLBACK TimerProc(HWND hWnd, UINT uMessage, UINT_PTR uEventId, DWORD dwTime)
-{
+{	
 	graphics->BeginDraw();
 	graphics->ClearScreen(0, 0, 0);
-	graphics->DrawRectangle(posX, posY, 20, 100, 0, 0, 1, 1);
+	graphics->DrawCircle(ballPosition.x, ballPosition.y, ballRadius, 0, 0, 1, 1);
+	//graphics->DrawRectangle(posX, posY, 20, 100, 0, 0, 1, 1);
 	graphics->EndDraw();
-	posX++;
+	ballPosition.x += ballSpeed.x;
+	ballPosition.y += ballSpeed.y;
+
+	BallBounceFromBoundary();
+	BallOutOfField();
 }
 
 void StartGame(HWND hwnd,bool isWithBotMode,int difficulty)
 {
+	RECT rect;
 	hWnd = hwnd;
 	graphics = new Graphics();
 	graphics->Init(hWnd);
+	//GetWindowRect(hWnd, &rect);
+	GetClientRect(hWnd, &rect);
+	windowHeight = rect.bottom - rect.top;
+	windowWidth = rect.right - rect.left;
+	ballSpeed.y = 10;
+	ballSpeed.x = 1;
+	ballPosition.x = (rect.right - rect.left) / 2;
+	ballPosition.y = (rect.bottom - rect.top) / 2;
 	SetTimer(hWnd, TIMER_ID, 20, TimerProc);
 }
 
@@ -34,8 +71,6 @@ void DeleteGameParams()
 
 void StopGame()
 {
-	posX = 0;
-	posY = 0;
 	DeleteGameParams();
 	SendMessage(hWnd, WM_COMMAND, EXIT_COMMAND, NULL);
 }
